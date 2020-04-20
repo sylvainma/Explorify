ea76f8419f663736d39cf3eca6b8c43c74c5bec4
Sylvain Marchienne <sylvain.marchienne@gmail.com>
2020-04-19 22:35:48 -0400

    Try to enforce no-cache

--- explorify_deploy/app.py
+++ explorify_deploy/app.py
@@ -81,3 +81,15 @@ def delete_city():
     city = json["city"]
     features_collection.delete_many({"city": city})
     return jsonify("Hello"), 200
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
